68c1eb4f65db21fa83ac30bc26407a6ab221439b
S.V. Novoselov <novoselovsv@bk.ru>
2024-08-14 18:13:58 +0300

    Correct installation postman, fonts and i3 config

--- setup.sh
+++ setup.sh
@@ -10,7 +10,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 brew install tmux
 brew install fzf
 brew install ripgrep
-brew install postman
+sudo snap install postman
+sudo apt install fonts-powerline
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 sudo /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
 sudo apt install ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
@@ -18,5 +21,6 @@ sudo apt update
 sudo apt install i3
 echo "source-file ~/vimrc/.tmux.conf.real" > ~/.tmux.conf
 echo "source ~/vimrc/.vimrc.real" > ~/.vimrc
+mkdir -p ~/.config/i3
 echo "include ~/vimrc/config.real" > ~/.config/i3/config
 cp .vimspector.json ~/.vimspector.json
